b253d710126effe6ac05ae437e28d8a05c57a6c6
ldodson911 <ldodson911@aol.com>
2017-01-15 18:10:36 -0600

    add child front page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -16,7 +16,6 @@ get_header(); ?>
 
 <section class="home-page">
 	<div class="site-content">
-		<?php query_posts('cat=3&year=2004'); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class='homepage-hero'>
 				<?php the_content(); ?>
@@ -26,4 +25,19 @@ get_header(); ?>
 	</div><!-- .container -->
 </section><!-- .home-page -->
 
+<section class="recent posts">
+	<div class="site-content">
+		<div class="blog-post">
+		<h4>FROM THE BLOG</h4>
+		<?php query_posts('posts_per_page=1') ?>
+			<?php while (have_posts() ) : the_post(); ?>
+				<h2><?php the_title(); ?></h2>
+				<?php the_excerpt() ?>
+				<a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
+
+			<?php endwhile; ?>
+		<?php wp_reset_query(); ?>
+		</div>
+	</div>
+</section>
 <?php get_footer(); ?>
